c072d62680841720121ffe598d92f481d90aa290
Artem Baguinski <a.baguinski@performation.com>
2021-04-12 07:00:58 +0200

    Refactor sync editor's changed

--- src/sync_editor.js
+++ src/sync_editor.js
@@ -1,22 +1,31 @@
 import React from "react"
 import Column from "./column"
 
+function min(array) {
+  return array.reduce((a, b) => Math.min(a, b))
+}
+
+function max(array) {
+  return array.reduce((a, b) => Math.max(a, b))
+}
+
 export default function SyncEditor({ data }) {
   const colRefs = data.map(() => React.useRef(null))
+
   function changed() {
-    const blocks = [
-      colRefs[0].current.querySelectorAll(".ce-block"),
-      colRefs[1].current.querySelectorAll(".ce-block"),
-    ]
-    const blockCount = Math.min(blocks[0].length, blocks[1].length)
+    const cols = colRefs.map(
+      ref => ref.current.querySelectorAll(".ce-block")
+    )
+    const blockCount = min(cols.map(col => col.length))
     for (let i = 0; i < blockCount; i++) {
-      const height = Math.max(
-        blocks[0][i].firstElementChild.clientHeight,
-        blocks[1][i].firstElementChild.clientHeight
+      const blocks = cols.map(col => col[i])
+      const height = max(
+        blocks.map(block => block.firstElementChild.clientHeight)
       )
-      height = "" + height + "px"
-      if (blocks[0][i]) { blocks[0][i].style.minHeight = height }
-      if (blocks[1][i]) { blocks[1][i].style.minHeight = height }
+      height = `${height}px`
+      blocks.forEach(block => {
+        block.style.minHeight = height
+      })
     }
   }
 
