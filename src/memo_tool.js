e2007e4286c4435c02eed944fd1ae6efc966ddf3
Artem Baguinski <a.baguinski@performation.com>
2021-04-17 19:39:00 +0200

    Start editing a memo after creating it

--- src/memo_tool.js
+++ src/memo_tool.js
@@ -1,12 +1,27 @@
 import Draggable from 'react-draggable'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
 import styles from '../styles/Memo.module.css'
 
 function Memo() {
+  const contentRef = React.useRef(null)
+  const [inited, setInited] = React.useState(false)
+
+  React.useEffect(() => {
+    const content = contentRef.current
+    if (!inited && content) {
+      const selection = window.getSelection()
+      selection.empty()
+      selection.collapse(content, 0)
+      setInited(true)
+    }
+  })
+
   return (
     <Draggable defaultPosition={{ x: -30, y: -20 }} >
       <div
+        ref={contentRef}
         className={styles.memo}
         style={{ minHeight: "3ex", minWidth: "2em" }}
       />
