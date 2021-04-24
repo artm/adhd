7e0944ee665cf976f4e0858179ee36502868677b
Artem Baguinski <a.baguinski@performation.com>
2021-04-24 09:20:31 +0200

    Switch memos.css to SCSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 import '../styles/editme.css'
-import '../styles/memos.css'
+import '../styles/memos.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
