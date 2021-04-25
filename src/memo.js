2c669ff6b17a57a8ffc41c6534436ff630e6f2c8
Artem Baguinski <a.baguinski@performation.com>
2021-04-25 11:53:27 +0200

    Pass memo content to the memoed data attribute

--- src/memo.js
+++ src/memo.js
@@ -5,6 +5,10 @@ export default function Memo({ memoedDom }) {
   const contentRef = React.useRef(null)
   const [inited, setInited] = React.useState(false)
 
+  function onContentEdited(event) {
+    memoedDom.dataset.memoText = this.innerText
+  }
+
   React.useEffect(() => {
     const content = contentRef.current
     if (!inited && content) {
@@ -12,18 +16,23 @@ export default function Memo({ memoedDom }) {
       selection.empty()
       selection.collapse(content, 0)
       setInited(true)
+      content.addEventListener("input", onContentEdited)
     }
   })
 
   return (
     <Draggable
       defaultPosition={{
-        x: memoedDom.offsetLeft,
-        y: memoedDom.offsetTop - 20
+        x: memoedDom.offsetLeft + 20,
+        y: memoedDom.offsetTop - 30
       }}
     >
-      <div ref={contentRef} className="memo">
-        <div contentEditable={true} className="memo-text" />
+      <div className="memo">
+        <div
+          ref={contentRef}
+          contentEditable={true}
+          className="memo-text"
+        />
         <div className="memo-button">×</div>
       </div>
     </Draggable>
